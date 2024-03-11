9a3d1a4bf641d6552b397f97e7dbe31305b4d3be
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-03-11 17:42:32 +0200

    Updates

---
+++ assets/Home-WRl4u_GU.js
@@ -0,0 +1,8 @@
+import{n as t,j as i}from"./index-Brs7PZEj.js";const n=t.div`
+  max-width: 1440px;
+  min-height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+`;function r(){return i.jsx(i.Fragment,{children:i.jsx("main",{children:i.jsx(n,{children:i.jsx("div",{children:i.jsx("h1",{style:{position:"absolute",top:"15%",right:"5%",color:"#0b0b0b",fontFamily:"Anta"},children:"This is Home page!!!"})})})})})}export{r as default};
---
+++ assets/ShoppingCart-BGfMCXjs.js
@@ -0,0 +1,8 @@
+import{n as i,j as t}from"./index-Brs7PZEj.js";const n=i.div`
+  max-width: 1440px;
+  min-height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+`;function a(){return t.jsx(t.Fragment,{children:t.jsx("main",{children:t.jsx(n,{children:t.jsx("div",{children:t.jsx("h1",{style:{position:"absolute",top:"15%",right:"5%",color:"#0b0b0b",fontFamily:"Anta"},children:"Shopping Cart"})})})})})}export{a as default};
